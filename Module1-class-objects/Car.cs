9871d88d865dfddfdabe6c49c47c2344fecdaeb6
msmun <msmun@SHAHRIAR-PC>
2025-08-22 00:28:51 +0200

    Add project files.

---
+++ Module1-class-objects/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_class_objects
+{
+    // Task 1: Create a Car Class
+    public class Car
+    {
+        
+        public  string Brand { get; set; }
+        public  string Model { get; set; }
+        public int ManufacturingYear { get; set; }
+        public int Mileage {  get; set; }
+
+        // Creating Drive method
+       public void Drive( int kilometers)
+        {
+            
+            // updating the mileage
+            Mileage += kilometers;
+            Console.WriteLine($"Car {Brand} {Model} driven {kilometers} km. Total mileage is now: {Mileage} km");
+        }
+
+
+        // Creating ShowCarinfo method
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"\nCar info: Brand - {Brand}, Model - {Model}, Year - {ManufacturingYear}, Mileage - {Mileage} \n");
+        }
+
+
+
+    }
+}
---
+++ Module1-class-objects/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Module1_class_objects;
+
+
+//Task 2: Create Objects of the Class
+// Creating first car object: Toyota
+// Prefered approach
+Car myToyota = new Car
+{
+    Brand = "Toyota",
+    Model = "Corolla",
+    ManufacturingYear = 2025,
+    Mileage = 50
+};
+
+// Access Drive method
+Console.WriteLine($"Initial mileage of {myToyota.Brand} {myToyota.Model} is: {myToyota.Mileage} km");
+myToyota.Drive(30);
+Console.WriteLine($"Total mileage of {myToyota.Brand} {myToyota.Model} is: {myToyota.Mileage} km");
+
+//Task 3: Accessing ShowCarInfo method
+myToyota.ShowCarInfo();
+
+
+// Creating second car object: Tesla
+// alternate approach
+
+Car myTesla = new Car();
+
+myTesla.Brand = "Tesla";
+myTesla.Model = "Model 3";
+myTesla.ManufacturingYear = 2025;
+myTesla.Mileage = 120;
+
+// Access Drive method
+Console.WriteLine($"Car {myTesla.Brand} {myTesla.Model} has driven {myTesla.Mileage} km");
+myTesla.Drive(30);
+Console.WriteLine($"Total mileage: {myTesla.Mileage}\n");
+
+
+//Task 3: Accessing ShowCarInfo method
+myTesla.ShowCarInfo();
+
+
+
