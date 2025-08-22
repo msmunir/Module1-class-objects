2d9334096cb2523930e178778f4576eeef712209
Mohammed Shahriar Munir <smunir07@gmail.com>
2025-08-22 12:09:57 +0200

    1. Add details of "NullReferenceException errors/Nullable reference type " warning 2. Add couple of alternative solution to avoid this error/warning.

--- Module1-class-objects/Car.cs
+++ Module1-class-objects/Car.cs
@@ -9,17 +9,25 @@ namespace Module1_class_objects
     // Task 1: Create a Car Class
     public class Car
     {
-        // I am coding the first property like this,
-        // Just for my own understanding and future reference
+        /* 
+        I am coding the first property like this,
+        Just for my own understanding and future reference.
 
+        Note:
+        Initialize property with default value. Why?
+        Because, Newer version of C# has nullable reference types enable.
+        So if not initialize Compiler will give warnings for nullable type.
+        Non-nullable - Must always have a value.
+        So,Initializing property with default value.
+        */
 
+        // First approach: Using Constructor.
         // field/variable
-        // Initialize property with default value.
-      
-        private string brand; 
+        private string brand = string.Empty;
 
-        // property
-        public string Brand     
+
+        // property 
+        public string Brand                
         {
             // get method
             // return the value of the variable/field brand.
@@ -31,11 +39,40 @@ namespace Module1_class_objects
         }
 
 
-        // Here I am using the shorcart version of above code
-        
-        public string Model { get; set; }
+        /*
+        Second approach: Make properties nullable
 
-      
+        Note: 
+        "?" nullable reference type modifier
+         Here The Model property can now hold either:
+            i) A valid string value(e.g., "Toyota","Corolla")
+            ii) null (no value)
+        */
+        public string? Model { get; set; }
+
+        #region
+        /*
+        Second approach: Shorcart version of First approach
+        public string Model { get; set; } = string.Empty;
+
+
+        Fourth approach: Initializing with default value
+        public string Model { get; set; } = "Unknown"; 
+
+        Fifth approach: (c# 11+) by using "required" modifier
+        public required string Model { get; set; }
+
+        Sixth approach: Using Constructor to ensure propertis are initialized
+        public Car( string brand, string model)
+        {
+            Brand = brand;
+            Model = model;
+        }
+        */
+        #endregion
+
+
+
         public int ManufacturingYear { get; set; }
         public int Mileage {  get; set; }
 
